770f38a348ef403815d886a92669fa177bf0cb5e
Dement0 <ilkeer.ozgur@gmail.com>
2019-10-19 19:25:45 +0200

    Add AddFlashcardActiviy

--- app/src/main/java/com/example/flashcards/MainActivity.kt
+++ app/src/main/java/com/example/flashcards/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.flashcards
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // Called when user taps the Add Flashcard button
+    fun addFlashcard(view: View) {
+        val intent = Intent(this, AddFlashcardActivity::class.java)
+        startActivity(intent)
     }
 }
