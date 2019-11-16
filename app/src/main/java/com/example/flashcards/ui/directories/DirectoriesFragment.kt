51946b6b79fd97fd5bd371aef65e3e4db298559a
Dement0 <ilkeer.ozgur@gmail.com>
2019-11-16 16:26:04 +0100

    Use ViewModel with Adapter

--- app/src/main/java/com/example/flashcards/ui/directories/DirectoriesFragment.kt
+++ app/src/main/java/com/example/flashcards/ui/directories/DirectoriesFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.example.flashcards.R
@@ -19,10 +20,7 @@ class DirectoriesFragment : Fragment() {
 
     private lateinit var viewModel: DirectoriesViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.directories_fragment, container, false)
     }
 
@@ -30,9 +28,15 @@ class DirectoriesFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(DirectoriesViewModel::class.java)
 
+        // LayoutManager and Adapter
         recyclerView_directories.layoutManager = LinearLayoutManager(this.context)
         recyclerView_directories.adapter = DirectoriesAdapter()
 
-    }
+        var directories_adapter = (recyclerView_directories.adapter as DirectoriesAdapter)
 
+        // Observer on directories_list variable
+        viewModel.directories_list.observe(this, Observer {
+            it.let { directories_adapter.directory_titles = it }
+        })
+    }
 }
--- app/src/main/java/com/example/flashcards/ui/directories/DirectoriesViewModel.kt
+++ app/src/main/java/com/example/flashcards/ui/directories/DirectoriesViewModel.kt
@@ -1,7 +1,26 @@
 package com.example.flashcards.ui.directories
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DirectoriesViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class   DirectoriesViewModel : ViewModel() {
+
+    private val directories_data = MutableLiveData<MutableList<String>>()
+
+    init {
+        directories_data.value = ArrayList()
+
+        addDirectoriesData("directory-1")
+        addDirectoriesData("directory-2")
+        addDirectoriesData("directory-3")
+        addDirectoriesData("directory-4")
+    }
+
+    val directories_list : LiveData<MutableList<String>> = directories_data
+
+    // Public method to add data
+    fun addDirectoriesData(directory: String) {
+        directories_data.value?.add(directory)
+    }
 }
