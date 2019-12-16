4f153c72ea6f12c68e4b47015817b8f77bb0b9db
Dement0 <ilkeer.ozgur@gmail.com>
2019-12-16 22:12:53 +0100

    Connect functionality to add_directory_button

--- app/src/main/java/com/example/flashcards/ui/directories/DirectoriesFragment.kt
+++ app/src/main/java/com/example/flashcards/ui/directories/DirectoriesFragment.kt
@@ -1,5 +1,7 @@
 package com.example.flashcards.ui.directories
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -13,6 +15,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.example.flashcards.R
 import com.example.flashcards.db.flashcard_directory.FlashcardDirectory
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.directories_fragment.*
 
 class DirectoriesFragment : Fragment() {
@@ -43,6 +46,40 @@ class DirectoriesFragment : Fragment() {
         recyclerView_directories.adapter = directoriesAdapter
 
         observeViewModel()
+
+        setupViews()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 2000 && resultCode == Activity.RESULT_OK) {
+            if (data != null) {
+                viewModel.send(
+                    DirectoryEvent.AddDirectory(
+                        FlashcardDirectory(
+                            id = 0,
+                            title = data.getStringExtra("ADD_DIRECTORY_TITLE_RESULT").toString(),
+                            creationDate = ""
+                        )
+                    )
+                )
+            } else {
+                Toast.makeText(context, "Error, no data.", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(context, "Error, please try again.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun setupViews() {
+        val addDirectoryButton: FloatingActionButton =
+            requireActivity().findViewById(R.id.add_directory_button)
+
+        addDirectoryButton.setOnClickListener {
+            val intent = Intent(activity, AddDirectoryScreen::class.java)
+            startActivityForResult(intent, 2000)
+        }
     }
 
     private fun observeViewModel() {
