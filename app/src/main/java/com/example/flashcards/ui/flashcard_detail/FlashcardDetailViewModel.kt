37ced07e6639662e7e20431feca5362d5e2abb2f
Dement0 <ilkeer.ozgur@gmail.com>
2020-02-24 22:42:10 +0100

    Load the image of a Flashcard (if exists) in review screen

--- app/src/main/java/com/example/flashcards/ui/flashcard_detail/FlashcardDetailViewModel.kt
+++ app/src/main/java/com/example/flashcards/ui/flashcard_detail/FlashcardDetailViewModel.kt
@@ -3,25 +3,20 @@ package com.example.flashcards.ui.flashcard_detail
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.flashcards.db.FlashcardDatabase
 import com.example.flashcards.db.flashcard.Flashcard
 import com.example.flashcards.db.flashcard.FlashcardRepository
 import kotlinx.coroutines.launch
 
-
-// Events that FlashcardDetailActivity can send
 sealed class FlashcardDetailEvent {
     object Load : FlashcardDetailEvent()
 }
 
-// States that FlashcardDetailViewModel can have
 sealed class FlashcardDetailState {
     data class Error(val error: Throwable) : FlashcardDetailState()
     data class Success(val flashcard: Flashcard) : FlashcardDetailState()
 }
-
 
 class FlashcardDetailViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -48,13 +43,6 @@ class FlashcardDetailViewModel(application: Application) : AndroidViewModel(appl
     private fun loadContent(flashcard_id: Int) = viewModelScope.launch {
         state.value = FlashcardDetailState.Success(
             repository.getFlashcard(flashcard_id)
-            /*Flashcard(
-                flashcard_id,
-                "Test Title",
-                null,
-                null,
-                null
-            )*/
         )
     }
 }
