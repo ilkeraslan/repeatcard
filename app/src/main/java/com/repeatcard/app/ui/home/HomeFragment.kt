6fb17d9eb7dcfe4edbd984545c8910bcccbe1e60
Dement0 <ilkeer.ozgur@gmail.com>
2020-04-19 11:52:45 +0200

    Remove logs and directories dependencies

--- app/src/main/java/com/repeatcard/app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/repeatcard/app/ui/home/HomeFragment.kt
@@ -4,33 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
-import android.widget.RadioButton
-import android.widget.RadioGroup
-import android.widget.ScrollView
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.repeatcard.app.R
-import com.repeatcard.app.db.directory.Directory
 import com.repeatcard.app.db.flashcard.Flashcard
-import com.repeatcard.app.ui.directories.DirectoriesViewModel
-import com.repeatcard.app.ui.notifications.NotificationEvent
-import com.repeatcard.app.ui.notifications.NotificationsViewModel
+import com.repeatcard.app.ui.flashcarddetail.FlashcardDetailActivity
 import com.repeatcard.app.ui.util.exhaustive
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.koin.android.ext.android.inject
 
 class HomeFragment : Fragment() {
 
-    @ExperimentalCoroutinesApi
-    private val notificationsViewModel: NotificationsViewModel by inject()
-    private val directoriesViewModel: DirectoriesViewModel by inject()
     private val homeViewModel: HomeViewModel by inject()
-
     private lateinit var homeAdapter: HomeAdapter
     private lateinit var homeListener: HomeListener
     private lateinit var recyclerView: RecyclerView
@@ -42,95 +30,22 @@ class HomeFragment : Fragment() {
     @ExperimentalCoroutinesApi
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        setUpRecyclerView()
         observeViewModel()
+        homeViewModel.send(FlashcardEvent.Load)
+        setUpRecyclerView(view)
     }
 
     @ExperimentalCoroutinesApi
-    private fun setUpRecyclerView() {
-        recyclerView = requireActivity().findViewById(R.id.recyclerView_home)
+    private fun setUpRecyclerView(view: View) {
+        recyclerView = view.findViewById(R.id.recyclerView_home)
         recyclerView.layoutManager = LinearLayoutManager(this.context)
         homeListener = object : HomeListener {
-            override fun itemDeleted(id: Int) {
-                alertToDelete(id)
-            }
-
-            override fun addFlashcardToDirectory(id: Int) {
-                if (getDirectories().isNotEmpty()) {
-                    alertToAdd(id)
-                } else showError(KotlinNullPointerException("No directory."))
+            override fun cardClicked(flashcard: Flashcard) {
+                FlashcardDetailActivity.openFlashcardDetailActivity(view.context, flashcard.id)
             }
         }
         homeAdapter = HomeAdapter(homeListener)
         recyclerView.adapter = homeAdapter
-    }
-
-    private fun getDirectories(): MutableList<Directory> {
-        val directoriesToAdd: MutableList<Directory> = mutableListOf()
-
-        directoriesViewModel.allDirectories.observe(
-            viewLifecycleOwner,
-            Observer { directory ->
-                directory.forEach { dir -> directoriesToAdd.add(dir) }
-            })
-
-        return directoriesToAdd
-    }
-
-    @ExperimentalCoroutinesApi
-    private fun alertToAdd(flashcardId: Int) {
-        val dialogBuilder = AlertDialog.Builder(requireContext())
-        val directories = getDirectories()
-
-        val radioGroup = RadioGroup(this.context)
-        val scroll = ScrollView(this.context)
-
-        directories.forEach { directory ->
-            val radioButton = RadioButton(this.context)
-            radioButton.id = directory.id
-            radioButton.text = directory.title
-            radioGroup.addView(radioButton)
-            radioButton.text
-        }
-
-        radioGroup.check(directories.first().id)
-
-        scroll.addView(radioGroup, RadioGroup.LayoutParams(WRAP_CONTENT, WRAP_CONTENT))
-
-        dialogBuilder.setTitle("Select Directory")
-        dialogBuilder.setPositiveButton("Select") { dialog, which ->
-            homeViewModel.send(FlashcardEvent.AddToDirectory(flashcardId, radioGroup.checkedRadioButtonId))
-            notificationsViewModel.send(NotificationEvent.AddToDirectory(flashcardId))
-        }
-        dialogBuilder.setNegativeButton("Cancel") { dialog, which -> dialog.cancel() }
-        dialogBuilder.setView(scroll).create().show()
-    }
-
-    private fun alertToDelete() {
-        val dialogBuilder = AlertDialog.Builder(requireContext())
-
-        dialogBuilder.setTitle("Are you sure you want to delete ALL?")
-        dialogBuilder.setPositiveButton("Yes") { dialog, which ->
-            homeViewModel.send(FlashcardEvent.DeleteAll)
-            Toast.makeText(context, "Deleted all.", Toast.LENGTH_SHORT).show()
-        }
-        dialogBuilder.setNegativeButton("No") { dialog, which -> homeViewModel.send(FlashcardEvent.Load) }
-        dialogBuilder.create().show()
-    }
-
-    @ExperimentalCoroutinesApi
-    private fun alertToDelete(id: Int) {
-        val dialogBuilder = AlertDialog.Builder(requireContext())
-
-        dialogBuilder.setTitle("Are you sure you want to delete this?")
-        dialogBuilder.setPositiveButton("Yes") { dialog, which ->
-            homeViewModel.send(FlashcardEvent.DeleteFlashcard(id))
-            notificationsViewModel.send(NotificationEvent.DeleteFlashcard(id))
-            Toast.makeText(context, "Deleted flashcard.", Toast.LENGTH_SHORT).show()
-        }
-        dialogBuilder.setNegativeButton("No") { dialog, which -> homeViewModel.send(FlashcardEvent.Load) }
-        dialogBuilder.create().show()
     }
 
     private fun observeViewModel() {
--- app/src/main/java/com/repeatcard/app/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/repeatcard/app/ui/home/HomeViewModel.kt
@@ -5,16 +5,12 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.repeatcard.app.db.FlashcardDatabase
-import com.repeatcard.app.db.directory.FlashcardDirectoryRepository
 import com.repeatcard.app.db.flashcard.Flashcard
 import com.repeatcard.app.db.flashcard.FlashcardRepository
+import com.repeatcard.app.ui.util.exhaustive
 import kotlinx.coroutines.launch
 
 sealed class FlashcardEvent {
-    data class AddFlashcard(val flashcard: Flashcard) : FlashcardEvent()
-    data class AddToDirectory(val id: Int, val directoryId: Int) : FlashcardEvent()
-    object DeleteAll : FlashcardEvent()
-    data class DeleteFlashcard(val id: Int) : FlashcardEvent()
     object Load : FlashcardEvent()
 }
 
@@ -26,52 +22,21 @@ sealed class FlashcardState {
 class HomeViewModel(context: Context) : ViewModel() {
 
     private val repository: FlashcardRepository
-    private val directoryRepository: FlashcardDirectoryRepository
     var state: MutableLiveData<FlashcardState> = MutableLiveData()
 
     init {
         val flashcardsDao = FlashcardDatabase.getDatabase(context).flashcardDao()
-        val directoryDao = FlashcardDatabase.getDatabase(context).directoryDao()
         repository = FlashcardRepository(flashcardsDao)
-        directoryRepository = FlashcardDirectoryRepository(directoryDao)
-        loadContent()
     }
 
     fun send(event: FlashcardEvent) {
         when (event) {
-            is FlashcardEvent.AddFlashcard -> insert(event.flashcard)
-            is FlashcardEvent.AddToDirectory -> addFlashcardToDirectory(event.id, event.directoryId)
-            is FlashcardEvent.DeleteAll -> deleteAll()
-            is FlashcardEvent.DeleteFlashcard -> deleteFlashcard(event.id)
             is FlashcardEvent.Load -> loadContent()
-        }
-    }
-
-    private fun addFlashcardToDirectory(flashcardId: Int, directoryId: Int) =
-        viewModelScope.launch {
-            val flashcardToChange = repository.getFlashcard(flashcardId)
-            flashcardToChange.directoryId = directoryId
-            repository.updateFlashcard(flashcardToChange)
-            loadContent()
-        }
-
-    private fun deleteAll() = viewModelScope.launch {
-        repository.deleteAll()
-        loadContent()
-    }
-
-    private fun deleteFlashcard(id: Int) = viewModelScope.launch {
-        repository.deleteFlashcard(id)
-        loadContent()
+        }.exhaustive
     }
 
     private fun loadContent() = viewModelScope.launch {
         val allFlashcards = repository.getFlashcards()
         state.postValue(FlashcardState.Success(allFlashcards))
-    }
-
-    private fun insert(flashcard: Flashcard) = viewModelScope.launch {
-        repository.insert(flashcard)
-        loadContent()
     }
 }
