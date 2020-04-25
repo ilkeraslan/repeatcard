4e1f260e8dc9c07c639b8c55d4ea6d76ef9aed27
Dement0 <ilkeer.ozgur@gmail.com>
2020-04-25 16:09:13 +0200

    Show OnboardingScreen based on prefs

--- app/src/main/java/it/ilker/repeatcard/ui/splash/SplashScreen.kt
+++ app/src/main/java/it/ilker/repeatcard/ui/splash/SplashScreen.kt
@@ -6,12 +6,14 @@ import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
 import androidx.appcompat.app.AppCompatActivity
 import it.ilker.repeatcard.R
 import it.ilker.repeatcard.ui.AppNavigator
+import it.ilker.repeatcard.ui.util.KeyValueStorage
 import kotlinx.coroutines.Runnable
 import org.koin.android.ext.android.inject
 
 class SplashScreen : AppCompatActivity() {
 
     private val navigator: AppNavigator by inject()
+    private val prefs: KeyValueStorage by inject()
 
     companion object {
         private const val DELAY = 2000L
@@ -26,7 +28,11 @@ class SplashScreen : AppCompatActivity() {
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
         Handler().postDelayed(Runnable {
-            navigator.goToOnboarding()
+            if (prefs.getBoolean("NEW USER", true)) {
+                navigator.goToOnboarding()
+            } else {
+                navigator.goToMain()
+            }
             finish()
         }, DELAY)
     }
