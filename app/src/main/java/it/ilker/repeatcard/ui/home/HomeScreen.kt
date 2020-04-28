2fab5fb7bb1f4e9c726bfdeab0381c6c3a3b15c6
Dement0 <ilkeer.ozgur@gmail.com>
2020-04-28 12:06:35 +0200

    Add placeholder texts in Home and Logs

--- app/src/main/java/it/ilker/repeatcard/ui/home/HomeScreen.kt
+++ app/src/main/java/it/ilker/repeatcard/ui/home/HomeScreen.kt
@@ -3,8 +3,10 @@ package it.ilker.repeatcard.ui.home
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -16,12 +18,13 @@ import it.ilker.repeatcard.ui.util.exhaustive
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.koin.android.ext.android.inject
 
-class HomeFragment : Fragment() {
+class HomeScreen : Fragment() {
 
     private val homeViewModel: HomeViewModel by inject()
     private lateinit var homeAdapter: HomeAdapter
     private lateinit var homeListener: HomeListener
     private lateinit var recyclerView: RecyclerView
+    private lateinit var noFlashcardText: TextView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.home_fragment, container, false)
@@ -32,11 +35,16 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         observeViewModel()
         homeViewModel.send(FlashcardEvent.Load)
-        setUpRecyclerView(view)
+        setupViews(view)
+        setupRecyclerView(view)
+    }
+
+    private fun setupViews(view: View) {
+        noFlashcardText = view.findViewById(R.id.noFlashcardTextHome)
     }
 
     @ExperimentalCoroutinesApi
-    private fun setUpRecyclerView(view: View) {
+    private fun setupRecyclerView(view: View) {
         recyclerView = view.findViewById(R.id.recyclerView_home)
         recyclerView.layoutManager = LinearLayoutManager(this.context)
         homeListener = object : HomeListener {
@@ -51,17 +59,21 @@ class HomeFragment : Fragment() {
     private fun observeViewModel() {
         homeViewModel.state.observe(viewLifecycleOwner, Observer { state ->
             when (state) {
-                is FlashcardState.Error -> showError(state.error)
+                is FlashcardState.Error -> showError()
                 is FlashcardState.Success -> showFlashcards(state.flashcards)
             }.exhaustive
         })
     }
 
-    private fun showError(error: Throwable) {
-        Toast.makeText(context, error.localizedMessage?.toString(), Toast.LENGTH_SHORT).show()
+    private fun showError() {
+        noFlashcardText.visibility = VISIBLE
+        homeAdapter.submitList(listOf())
+        homeAdapter.notifyDataSetChanged()
     }
 
     private fun showFlashcards(flashcards: List<Flashcard>) {
+        noFlashcardText.visibility = INVISIBLE
         homeAdapter.submitList(flashcards)
+        homeAdapter.notifyDataSetChanged()
     }
 }
