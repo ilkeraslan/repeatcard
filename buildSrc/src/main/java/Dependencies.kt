e0bb45cf9cf29933792cdf5657f977ca016b2e87
Ilker Aslan <ilkeer.ozgur@gmail.com>
2020-09-29 08:03:52 +0200

    Bump version to 1.1.1 (#83)

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -2,8 +2,8 @@
 object Versions {
 
     //app
-    const val versionCode = 2 //used for local builds, CI uses codes used for published versions
-    const val versionName = "1.0.0"
+    const val versionCode = 3 //used for local builds, CI uses codes used for published versions
+    const val versionName = "1.1.1"
 
     // platform
     const val buildTools = "29.0.2"
