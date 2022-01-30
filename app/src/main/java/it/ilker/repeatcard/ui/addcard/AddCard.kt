ec13c6b9bac978c8d2d17eaaf3e84aa9b3a3d88c
Ilker Aslan <ilkerozguraslan@gmail.com>
2022-01-30 19:19:46 +0100

    Pick photo

--- app/src/main/java/it/ilker/repeatcard/ui/addcard/AddCard.kt
+++ app/src/main/java/it/ilker/repeatcard/ui/addcard/AddCard.kt
@@ -1,9 +1,11 @@
 package it.ilker.repeatcard.ui.addcard
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
@@ -15,8 +17,11 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,14 +30,18 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun AddCard(
     modifier: Modifier = Modifier,
+    onSelectImage: () -> Unit = {},
+    onImageSelected: ImageBitmap? = null,
     onAdded: (String) -> Unit = {},
     onBackPressed: () -> Unit = {}
 ) {
     val textState = remember {
-        mutableStateOf(TextFieldValue(annotatedString = AnnotatedString("")))
+        mutableStateOf(
+            TextFieldValue(annotatedString = AnnotatedString(""))
+        )
     }
 
-    Column(modifier = modifier) {
+    Column(modifier = modifier, verticalArrangement = Arrangement.SpaceBetween) {
         OutlinedTextField(
             modifier = Modifier.fillMaxWidth(),
             value = textState.value,
@@ -40,6 +49,27 @@ fun AddCard(
                 textState.value = it
             }
         )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        onImageSelected?.let { bitmap ->
+            Image(
+                modifier = Modifier.align(CenterHorizontally),
+                bitmap = bitmap,
+                contentDescription = ""
+            )
+        } ?: kotlin.runCatching {
+            OutlinedButton(
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.textButtonColors(
+                    backgroundColor = Color.LightGray,
+                    contentColor = Color.White
+                ),
+                onClick = onSelectImage
+            ) {
+                Text(text = "Select an image")
+            }
+        }
 
         Spacer(modifier = Modifier.height(20.dp))
 
@@ -85,6 +115,6 @@ fun AddCard(
 @Composable
 private fun AddCardPreview() {
     AddCard(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier.fillMaxSize(),
     )
 }
--- app/src/main/java/it/ilker/repeatcard/ui/addcard/AddCardFactory.kt
+++ app/src/main/java/it/ilker/repeatcard/ui/addcard/AddCardFactory.kt
@@ -1,12 +1,19 @@
 package it.ilker.repeatcard.ui.addcard
 
+import android.graphics.Bitmap
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.launch
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
@@ -27,16 +34,24 @@ object AddCardFactory : NavFactory {
         val vm by viewModel<AddCardViewModel>()
         val state = vm.state.collectAsState()
 
+        val result = remember { mutableStateOf<Bitmap?>(null) }
+        val launcher = rememberLauncherForActivityResult(
+            ActivityResultContracts.TakePicturePreview()
+        ) { bitmap ->
+            result.value = bitmap
+        }
+
         when (state.value) {
             AddCardState.Initial -> AddCard(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(25.dp),
+                onSelectImage = { launcher.launch() },
+                onImageSelected = result.value?.asImageBitmap(),
                 onAdded = { title ->
                     vm.send(AddCardEvent.Add(title))
-                },
-                onBackPressed = { navController.popBackStack() }
-            )
+                }
+            ) { navController.popBackStack() }
             AddCardState.Error -> Error(
                 modifier = Modifier.fillMaxSize()
             )
