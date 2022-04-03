ee9898fd97c94361e98a780bf84dab58c07463f4
Ilker Aslan <ilkerozguraslan@gmail.com>
2022-04-03 13:44:43 +0200

    Add preview

--- design/src/main/java/me/ilker/design/quiz/Results.kt
+++ design/src/main/java/me/ilker/design/quiz/Results.kt
@@ -4,12 +4,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import me.ilker.business.quiz.QuizResult
+import me.ilker.design.R
 
 @Composable
 fun Results(
@@ -23,11 +27,46 @@ fun Results(
                 .align(Alignment.Center),
             verticalArrangement = Arrangement.spacedBy(20.dp)
         ) {
-            Text(text = result.questions.size.toString())
+            Text(
+                text = stringResource(
+                    id = R.string.results,
+                    result.questions.size.toString()
+                )
+            )
 
-            Text(text = result.correctAnswers.size.toString())
+            Text(
+                text = stringResource(
+                    id = R.string.correct_answers,
+                    result.correctAnswers.size.toString()
+                )
+            )
 
-            Text(text = result.wrongAnswers.size.toString())
+            Text(
+                text = stringResource(
+                    id = R.string.wrong_answers,
+                    result.wrongAnswers.size.toString()
+                )
+            )
         }
     }
+}
+
+/**
+ * Previews
+ */
+@ExperimentalMaterialApi
+@Preview(
+    backgroundColor = 0xFFFFFF,
+    showBackground = true
+)
+@Composable
+private fun ResultsPreview() {
+    Results(
+        result = QuizResult(
+            id = "5",
+            questions = listOf(),
+            correctAnswers = listOf(),
+            wrongAnswers = listOf()
+        )
+    )
 }
